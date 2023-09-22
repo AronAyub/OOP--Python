5b4aef924a6ddc0fd1b2aabe5c9d1f8554180702
Aron Ayub <55284959+AronAyub@users.noreply.github.com>
2023-09-22 23:37:57 +0300

    understood interfaces

--- Programs/chapter2/class_interface.py
+++ Programs/chapter2/class_interface.py
@@ -1,7 +1,13 @@
-#base class that defines tempate for other classes to inherit from
-#python oob
-#Using Abstract base classes to enforce class constraints.
+#how to inherit from more than one class
+#interface is a type of programming feature that is supported using classes 
+#a class makes a promise to provide a capability
 from abc import ABC, abstractmethod
+class JSONify(ABC):
+     @abstractmethod
+     def toJSON(self):
+          pass
+
+
 class Graphics(ABC):
     def __init__(self):
         super().__init__()
@@ -10,12 +16,15 @@ class Graphics(ABC):
     def CalcArea(self):
             pass
 
-class Circle(Graphics):
+class Circle(Graphics, JSONify):
     def __init__(self, radius) :
         self.radius = radius
 
     def CalcArea(self):
          return 3.14 * (self.radius ** 2)
+    
+    def toJSON(self):
+         return f"{{\"Circle\" : {str(self.CalcArea())} }}"
 
 class Square(Graphics):
     def __init__(self, side):
@@ -28,6 +37,7 @@ class Square(Graphics):
 
 c = Circle(7)
 print(c.CalcArea)
-s = Square(3)
-print(s.CalcArea())
- 
+# s = Square(3)
+# print(s.CalcArea)
+
+print(c.toJSON())
